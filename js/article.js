93ecbe3e91cc3f6d2aa70916af72081920fcdd6a
anupent <anupent@gmail.com>
2018-04-01 19:59:10 +0545

    Update article.js

--- js/article.js
+++ js/article.js
@@ -62,3 +62,14 @@ $("#goto-content").click(function () {
    $("#nav-home-tab").addClass("active");
    window.scrollTo(0, 0);
 });
+
+/* clicking anywhere in the article-detail page removes author popover */
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
